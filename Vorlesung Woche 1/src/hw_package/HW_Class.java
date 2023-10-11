14ba6bc740d48316532efd2333f6d0ccda005280
kas35258 <simicat@icloud.com>
2023-10-11 19:51:41 +0200

    Vorlesung Woche 1 Ordner

---
+++ Vorlesung Woche 1/src/hw_package/HW_Class.java
@@ -0,0 +1,15 @@
+package hw_package;
+
+public class HW_Class {
+
+
+	     
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+			//src/main/java: alle code zeilen. für compilierung
+			//src/main/ressources: Bilder, alles nicht java codiges
+			//kein .idea datei. schlecht practice. in .gitignore lieber
+	
+	}
+
+}
